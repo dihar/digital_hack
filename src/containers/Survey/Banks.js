11b6dd6b7d7766abb800d844f1c5a6a95b119a78
dihar <diharin@gmail.com>
2019-07-28 16:14:45 +0300

    add links

--- src/containers/Survey/Banks.js
+++ src/containers/Survey/Banks.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import banksSrc from './resources/bank.jpeg';
+import linksSrc from './resources/text.jpeg';
 import {
   BanksStyled
 } from './styles';
 
-const Banks = () => (
-  <BanksStyled><img src={banksSrc} alt='banks'/></BanksStyled>
-);
+const Banks = () => {
+  const [mode, setMode] = useState(0);
+
+  return (
+    <BanksStyled>
+      {mode === 0 && <img onClick={() => setMode(1)} src={banksSrc} alt='banks'/>}
+      {mode === 1 && <img src={linksSrc} alt='banks'/>}
+    </BanksStyled>
+  );
+};
 
 export default Banks;
